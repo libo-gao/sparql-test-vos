07d2f11fbe7011b7b5aa73352e3b5f33004cf7a7
nosrepus <nosrepus@gmail.com>
2016-02-05 22:39:00 +0000

    pair warmup and test

--- post-query.py
+++ post-query.py
@@ -3,22 +3,42 @@ import time
 
 headers = {"Content-type": "application/sparql",
             "Accept": "text/plain"}
-path = "stress-workloads/watdiv-stress-100/test.1.sparql"
+path = "stress-workloads/watdiv-stress-100/warmup.sparql"
 sqlfile = open(path, 'r')
-result = open("result-100M/test1", "w")
-print "start now..."
+result = open("result-100M/warm", "w")
+print "warmup start now..."
 line = sqlfile.readline()
 while line:
 	index = line.find('WHERE')
 	output_line = line[:index] + 'FROM <http://watdiv.10M> ' + line[index:]
 	start = time.time()
-	r = requests.post("http://localhost:8890/sparql", data = {"query":output_line}, timeout = 60000)
+	r = requests.post("http://localhost:8890/sparql", data = {"query":output_line}, timeout = 60)
 	#print r.text
 	end = time.time()
 	result.write(str(end - start))
 	result.write('\n')
 	#print("%.03f" %(end-start))
 	line = sqlfile.readline()
-print "end successfully."
+print "warmup end successfully."
+sqlfile.close()
+result.close()
+
+path = "stress-workloads/watdiv-stress-100/test.1.sparql"
+sqlfile = open(path, 'r')
+result = open("result-100M/test1", "w")
+print "test start now..."
+line = sqlfile.readline()
+while line:
+        index = line.find('WHERE')
+        output_line = line[:index] + 'FROM <http://watdiv.10M> ' + line[index:]
+        start = time.time()
+        r = requests.post("http://localhost:8890/sparql", data = {"query":output_line}, timeout = 60)
+        #print r.text
+        end = time.time()
+        result.write(str(end - start))
+        result.write('\n')
+        #print("%.03f" %(end-start))
+        line = sqlfile.readline()
+print "test end successfully."
 sqlfile.close()
 result.close()
