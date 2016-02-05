3108642148f7a7bab427da44481ccf2326bf53c6
root <root@6dbe8340955e.(none)>
2016-02-05 01:44:10 +0000

    use time.time() instead time.clock()

--- post-query.py
+++ post-query.py
@@ -3,20 +3,22 @@ import time
 
 headers = {"Content-type": "application/sparql",
             "Accept": "text/plain"}
-path = "test.3.sparql"
+path = "stress-workloads/watdiv-stress-100/test.1.sparql"
 sqlfile = open(path, 'r')
-result = open("result100/test3", "w")
+result = open("result-100M/test1", "w")
+print "start now..."
 line = sqlfile.readline()
 while line:
 	index = line.find('WHERE')
-	output_line = line[:index] + 'FROM <http://watdiv.10m> ' + line[index:]
-	start = time.clock()
+	output_line = line[:index] + 'FROM <http://watdiv.10M> ' + line[index:]
+	start = time.time()
 	r = requests.post("http://localhost:8890/sparql", data = {"query":output_line}, timeout = 60000)
-	end = time.clock()
+	#print r.text
+	end = time.time()
 	result.write(str(end - start))
 	result.write('\n')
 	#print("%.03f" %(end-start))
 	line = sqlfile.readline()
-	
+print "end successfully."
 sqlfile.close()
 result.close()
